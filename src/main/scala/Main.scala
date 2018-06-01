51c8a08c4e2419f1983e6b28230754b4115f7b97
PK <pradhyu.shrestha@gmail.com>
2018-05-31 21:22:49 -0400

    Multiple SSH login in repl is throwing error

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,13 +2,24 @@ import com.decodified.scalassh._
 import org.bouncycastle.jce.provider._
 
 object Main extends App {
-// using host config 
+  // using host config
   SSH("localhost", HostConfigProvider.fromHostConfig(
-    new HostConfig( new PasswordLogin("root",PasswordProducer.fromString( "root")), hostName= "localhost", port= 2222))) { 
+    new HostConfig( new PasswordLogin("root",PasswordProducer.fromString( "root")), hostName= "localhost", port= 2222))) {
     client =>
-    client.exec("ls /").map { result => 
+    client.exec("ls /").map { result =>
       println("Result:\n" + result.stdOutAsString())
     }
+    client.close()
   }
 
+  SSH("localhost", HostConfigProvider.fromHostConfig(
+    new HostConfig( new PasswordLogin("root",PasswordProducer.fromString( "root")), hostName= "localhost", port= 2222))) {
+    client =>
+    client.exec("touch ~/zyz")
+    client.exec("ls ~/").map {result =>
+      println ("result: \n" + result.stdOutAsString())
+    }
+    client.close()
+
+  }
 }
