501f5015bc5c9cacb5dfbcabf18d510f5964a67f
PK <pradhyu.shrestha@gmail.com>
2018-05-31 21:13:01 -0400

    Initial commit

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,14 @@
+import com.decodified.scalassh._
+import org.bouncycastle.jce.provider._
+
+object Main extends App {
+// using host config 
+  SSH("localhost", HostConfigProvider.fromHostConfig(
+    new HostConfig( new PasswordLogin("root",PasswordProducer.fromString( "root")), hostName= "localhost", port= 2222))) { 
+    client =>
+    client.exec("ls /").map { result => 
+      println("Result:\n" + result.stdOutAsString())
+    }
+  }
+
+}
